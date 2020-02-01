f65056e729b395d94e22dc02563d06bc97974c77
chen <578159913@qq.com>
2020-02-01 19:42:23 +0800

    设置路由

---
+++ src/application/Rank/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Rank () {
+
+  return (
+    <div>
+      <div>Rank</div>
+    </div>
+  )
+}
+
+export default React.memo (Rank);
---
+++ src/application/Singers/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Recommend () {
+
+  return (
+    <div>
+      <div>Recommend</div>
+    </div>
+  )
+}
+
+export default React.memo (Recommend);
